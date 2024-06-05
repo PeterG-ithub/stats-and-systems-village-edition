0ce7c13a149cee259cb433d15e81efe06844f290
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-04 19:23:11 -0700

    initial commit
    - Setup initial project structure
    - Testing class creation and imports

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.human import Human
+
+
+def main():
+    alice = Human("Alice", 25)
+    bob = Human("Bob", 22)
+
+    print(alice)
+    print(bob)
+
+
+if __name__ == "__main__":
+    main()
