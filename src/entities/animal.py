303559bb30ae1c237f74b29e0e7051d7046dc638
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-06 19:08:19 -0700

    Add more variables to animal class

--- src/entities/animal.py
+++ src/entities/animal.py
@@ -1,10 +1,16 @@
 from src.event_simulator import Dice
+import random
 
 
 class Animal:
     def __init__(self, id, species="") -> None:
         self.id = id
         self.species = species
+        self.age = 0  # Age in days
+        self.mortality_rate = 0
+        self.is_female = random.choice([True, False])
+        self.is_pregnant = False
+        self.gestation_timer = 0
 
     def __str__(self) -> str:
         return f"{self.species}#{self.id} rolled {self.roll()}"
