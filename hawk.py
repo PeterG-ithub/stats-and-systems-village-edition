3369015375a8d7291a81c07e08a68195dbd611f1
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-13 20:31:15 -0700

    Add function to hawk
    - Add set_hawk_attributes function

--- hawk.py
+++ hawk.py
@@ -1,5 +1,9 @@
 from src.entities.animal import Animal
 
+
 class Hawk(Animal):
     def __init__(self, id, species="Hawk") -> None:
         super().__init__(id, species)
+
+    def set_hawk_attributes(self):
+        pass
